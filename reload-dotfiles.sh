b09324937411629ea68c997192889bc86e576dca
Cole Caccamise <colecaccamise@gmail.com>
2025-02-12 07:15:24 -0500

    add raycast reload script

---
+++ reload-dotfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Reload dotfiles
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author user_fecd0ccf6927f61b7898
+# @raycast.authorURL https://raycast.com/user_fecd0ccf6927f61b7898
+
+# run config script to reload dotfiles
+sh ~/dotfiles/config.sh
